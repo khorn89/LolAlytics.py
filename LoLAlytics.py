7095baf1362244e4f2312d077a32f3e75ab928f7
m-marqx <124513922+m-marqx@users.noreply.github.com>
2023-05-05 14:42:15 -0300

    fix: fix TimeoutException error

--- LoLAlytics.py
+++ LoLAlytics.py
@@ -8,11 +8,12 @@ import csv
 driver = webdriver.Chrome()
 url = "https://lolalytics.com/lol/tierlist/"
 
-file = open("TierlistPatch10.10.csv", 'w')
+file = open("TierlistPatch.csv", 'w')
 writer = csv.writer(file)
 
 driver.get(url)
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div[9]/div[2]")))
+local_class = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div[6]/div/div")))
+element_class = local_class.get_attribute("class")
 
 #List of attributes that are getting scrapped.
 name=[]
@@ -25,14 +26,13 @@ writer.writerow(['Champion Name', 'Win rate', 'Pick Rate'])
 content = driver.page_source
 soup = BeautifulSoup(content)
 
-for row in soup.find('div', 'tierlist wrapper').find_all('div', recursive=False):
+for row in soup.find('div', attrs={'class': element_class}).find_all('div', recursive=False):
     columns = row.find_all('div')
-    name = columns[3].text
-    winRate = columns[8].text
-    pickRate = columns[10].text
+    name = columns[1].text
+    winRate = columns[6].text
+    pickRate = columns[3].text
 
     print(f'{name} - {winRate} - {pickRate}')
     writer.writerow([name, winRate, pickRate])
 
 driver.close()
-
